b96c2e3b627640d5a89dd831d4c7fd39f27a41a7
Smartucheli <savio.martucheli@gmail.com>
2024-05-14 22:49:07 -0300

    Primeiro commit

---
+++ src/data/questions-complete.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+      category: "Futebol",
+      questions: [
+        {
+          question: "Qual Melhor Jogador de Todos os Tempos?",
+          options: ["Cristiano Ronaldo", "Pele", "Matheus Pereira", "Messi"],
+          answer: "Cristiano Ronaldo",
+          tip: "É uma besta enjaulada",
+        },
+        {
+          question: "Qual Time Ganhou a Copa do Mundo de 2003",
+          options: ["Brasil", "Alemanha", "Espanha", "Não Teve Copa"],
+          answer: "Não Teve Copa",
+        },
+        {
+          question: "Qual Maior Time de Minas",
+          options: ["America Mineiro", "Tombense", "Cruzeiro", "Villa Nova"],
+          answer: "Cruzeiro",
+        },
+        {
+          question: "Qual o Minimo de Jogadores para ter uma Partida de Futebol",
+          options: ["10", "8", "6", "7"],
+          answer: "7",
+        },
+      ],
+    },
+    {
+      category: "Carros",
+      questions: [
+        {
+          question: "Qual a Melhor Marca de Carros Entre as Opções?",
+          options: ["Ferrari", "porsche", "Lamborghini", "McLaren"],
+          answer: "Ferrari",
+          tip: "Esta na Formula 1",
+        },
+        {
+          question: "Entre os Carros Qual é o melhor?",
+          options: ["Uno", "Marea Turbo", "Fusca", "Honda Fit"],
+          answer: "Uno",
+        },
+        {
+          question: "Qual dessas marcas não é do grupo volkswagen",
+          options: ["Audi", "Porsche", "Bentley", "BMW"],
+          answer: "BMW",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
